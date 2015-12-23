c442a2598d3524202e063c6704962daab326fde6
Eric Sage <eric.david.sage@gmail.com>
2015-12-22 21:38:26 -0800

    Make lint not fail npm

--- src/state/reducers/styleSelectorReducer.js
+++ src/state/reducers/styleSelectorReducer.js
@@ -23,7 +23,6 @@ export default function styleSelector(state = defaultStyles, action) {
             return state;
 
         case REMOVE_STYLE:
-            console.log("Deleting...");
             return state;
 
         default:
